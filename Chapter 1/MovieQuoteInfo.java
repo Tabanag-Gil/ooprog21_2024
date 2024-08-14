460520789d3c59bc999a4ee214c414025e8d609a
Gil A. Tabañag <Giltabanag.important@gmail.com>
2024-08-14 18:15:42 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,8 @@
+class MovieQuoteInfo {
+   public static void main(String[] args){
+     System.out.println("Movie Quote: \"Sometimes we need to be apart to understand just how much we truly love each other\"");
+     System.out.println("Movie: The NoteBook");
+     System.out.println("Character: Noah");
+     System.out.println("Year of the Movie: 2004");
+   }
+}
