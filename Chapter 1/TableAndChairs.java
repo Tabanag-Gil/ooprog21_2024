b047ef769494176b6a31d7dfaba85efd258c3688
Gil A. Tabañag <Giltabanag.important@gmail.com>
2024-08-14 18:28:25 +0800

    Add files via upload

---
+++ Chapter 1/TableAndChairs.java
@@ -0,0 +1,10 @@
+class TableAndChairs {
+   public static void main(String [] args){
+   System.out.println("X                      X\n" +
+                      "X                      X\n" +
+                      "X      XXXXXXXXXX      X\n" +
+                      "XXXXX  X        X  XXXXX\n" +
+                      "X   X  X        X  X   X\n" +
+                      "X   X  X        X  X   X");
+   }
+}
