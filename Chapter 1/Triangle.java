5b6217633fa7c21653caf9998d8317c37448a999
Gil A. Tabañag <Giltabanag.important@gmail.com>
2024-08-14 19:20:11 +0800

    Add files via upload

---
+++ Chapter 1/Triangle.java
@@ -0,0 +1,11 @@
+class Triangle {
+   public static void main(String [] args){
+   System.out.println("       I\n" +
+                      "      III\n" +
+                      "     IIIII\n" +
+                      "    IIIIIII\n" +
+                      "   IIIIIIIII\n" +
+                      "  IIIIIIIIIII\n" +
+                      " IIIIIIIIIIIII\n" );
+   }
+}
