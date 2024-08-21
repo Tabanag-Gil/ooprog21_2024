b8321afdd4f4b40b2a365b16ca0fe9ea3fbc0b07
Gil A. Tabañag <Giltabanag.important@gmail.com>
2024-08-21 18:16:35 +0800

    Add files via upload

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class SwingAlert {
+    public static void main(String[] args) {
+        String message = String.format("Hello!, My name is Gil and you can call me Giru! :)");
+        JOptionPane.showMessageDialog(null, message);
+    }
+}
