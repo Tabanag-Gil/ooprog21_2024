988ada7d7a7ef118a101ecd6721c5ac787646db2
Gil A. Tabañag <Giltabanag.important@gmail.com>
2024-09-06 09:38:19 +0700

    Create prelim.java

---
+++ Prelim/prelim.java
@@ -0,0 +1,14 @@
+ public class feBo{
+      public static void main (String[]args){
+      int num = 10;
+      int a = 0;
+      int b = 1;
+    for (int i = 0; i <=num; i++){
+          System.out.printf("\nFibonacci(%d) -> %d",i,a);
+            int nexTerm = a + b;
+            a = b;
+            b = nexTerm ;
+                    
+         }
+      }
+   }
