adae261c8141eb08986764ef8b94361a16aaa04a
Gil A. Tabañag <Giltabanag.important@gmail.com>
2024-10-02 16:26:30 +0800

    Create MathTest.java

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,14 @@
+class MathTest {
+    public static void main(String[] args) {
+     System.out.println("SQRT:  "+ Math.sqrt(36));
+     System.out.println("SIN:  "+ Math.sin(36));
+     System.out.println("COS:  "+ Math.cos(36));
+     System.out.println("FLOOR:  "+ Math.floor(36.5));
+     System.out.println("CEIL:  "+ Math.ceil(36.5));
+     System.out.println("ROUND:  "+ Math.round(36.5));
+     System.out.println("MAX:  "+ Math.max(36,37));
+     System.out.println("MIN:  "+ Math.min(36,37));
+     System.out.println("RANDOM:  "+ Math.random());
+     
+    }
+}
